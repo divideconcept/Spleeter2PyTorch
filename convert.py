04ec8b26a46aa7e36a737c88b9464fed39c2c2f5
Robin Lobel <divide@divideconcept.net>
2021-12-29 22:41:13 +0100

    initial commit

---
+++ convert.py
@@ -0,0 +1,192 @@
+input_folders={2:'2stems', 4:'4stems', 5:'5stems'}
+output_folder=''
+unmix_example='unmix.wav' #must be a wav file
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from unet import UNet
+from util import tf2pytorch
+
+import math
+import torchaudio
+
+def load_ckpt(model, ckpt):
+    state_dict = model.state_dict()
+    for k, v in ckpt.items():
+        if k in state_dict:
+            target_shape = state_dict[k].shape
+            assert target_shape == v.shape
+            state_dict.update({k: torch.from_numpy(v)})
+        else:
+            print('  ignore', k)
+
+    model.load_state_dict(state_dict)
+    return model
+
+def pad_and_partition(tensor, T):
+    """
+    pads zero and partition tensor into segments of length T
+
+    Args:
+        tensor(Tensor): BxCxFxL
+
+    Returns:
+        tensor of size (B*[L/T] x C x F x T)
+    """
+    old_size = tensor.size(3)
+    new_size = math.ceil(old_size/T) * T
+    tensor = F.pad(tensor, [0, new_size - old_size])
+    [b, c, t, f] = tensor.shape
+    split = new_size // T
+    return torch.cat(torch.split(tensor, T, dim=3), dim=0)
+
+
+class SpleeterNet(nn.Module):
+    def __init__(self, num_instruments, checkpoint_path):
+        super().__init__()
+
+        self.num_instruments=num_instruments
+
+        ckpts = tf2pytorch(checkpoint_path, num_instruments)
+
+        for i in range(self.num_instruments):
+            sub_model = UNet(elu=False if num_instruments==2 else True)
+            sub_model.eval()
+            load_ckpt(sub_model, ckpts[i])
+            self.__setattr__('sub_model'+str(i), sub_model)
+
+    def forward(self, x):
+        result = []
+
+        outputs = []
+        for i in range(self.num_instruments):
+            output=self.__getattr__('sub_model'+str(i))(x)
+            outputs.append(output)
+
+        outputs = torch.stack(outputs, dim=-1)
+
+        if self.num_instruments==5:
+            mask = torch.softmax(outputs, dim=-1)
+        else:
+            mask = torch.sigmoid(outputs)
+
+        for i in range(self.num_instruments):
+            result.append(mask[..., i])
+
+        return tuple(result)
+
+
+#OPTIONAL
+class Estimator(nn.Module):
+    def __init__(self, model):
+        super(Estimator, self).__init__()
+
+        self.model=model
+        self.model.eval()
+
+        # stft config
+        self.F = 1024
+        self.T = 512
+        self.win_length = 4096
+        self.hop_length = 1024
+        self.win = torch.hann_window(self.win_length)
+
+    def compute_stft(self, wav):
+        """
+        Computes stft feature from wav
+
+        Args:
+            wav (Tensor): B x L
+        """
+
+        stft = torch.stft(
+            wav, self.win_length, hop_length=self.hop_length, window=self.win)
+
+        # only keep freqs smaller than self.F
+        stft = stft[:, :self.F, :, :]
+        real = stft[:, :, :, 0]
+        im = stft[:, :, :, 1]
+        mag = torch.sqrt(real ** 2 + im ** 2)
+
+        return stft, mag
+
+    def inverse_stft(self, stft):
+        """Inverses stft to wave form"""
+
+        pad = self.win_length // 2 + 1 - stft.size(1)
+        stft = F.pad(stft, (0, 0, 0, 0, 0, pad))
+        wav = torch.istft(stft, self.win_length, hop_length=self.hop_length,
+                    window=self.win)
+        return wav.detach()
+
+    def separate(self, wav, separation_exponent=1):
+        """
+        Separates stereo wav into different tracks corresponding to different instruments
+
+        Args:
+            wav (tensor): 2 x L
+        """
+
+        # stft - 2 X F x L x 2
+        # stft_mag - 2 X F x L
+        stft, stft_mag = self.compute_stft(wav)
+
+        L = stft.size(2)
+
+        # 1 x 2 x F x T
+        stft_mag = stft_mag.unsqueeze(-1).permute([3, 0, 1, 2])
+        stft_mag = pad_and_partition(stft_mag, self.T)  # B x 2 x F x T
+        stft_mag = stft_mag.transpose(2, 3)  # B x 2 x T x F
+
+        B = stft_mag.shape[0]
+
+        # compute instruments' mask
+        masks = self.model(stft_mag)
+
+        # compute denominator
+        mask_sum = sum([m ** 2 for m in masks])
+        mask_sum += 1e-10
+
+        wavs = []
+        for mask in masks:
+            mask = (mask ** separation_exponent + 1e-10/2)/(mask_sum)
+            mask = mask.transpose(2, 3)  # B x 2 X F x T
+
+            mask = torch.cat(
+                torch.split(mask, 1, dim=0), dim=3)
+
+            mask = mask.squeeze(0)[:,:,:L].unsqueeze(-1) # 2 x F x L x 1
+            stft_masked = stft * mask
+            wav_masked = self.inverse_stft(stft_masked)
+
+            wavs.append(wav_masked)
+
+        return wavs
+
+
+for input_folder in input_folders:
+    print("")
+    print("loading "+str(input_folder)+" stems model...")
+    model=SpleeterNet(num_instruments=input_folder, checkpoint_path=input_folders[input_folder])
+    model.eval()
+
+    if unmix_example:
+        print("separating...")
+        es = Estimator(model)
+        wav, sr = torchaudio.load(unmix_example) # load wav audio
+        wav_torch = wav / (wav.max() + 1e-8) # normalize audio
+        wavs = es.separate(wav_torch)
+        for i in range(len(wavs)):
+            fname = str(input_folder)+'stems_out_{}.wav'.format(i)
+            print('  writing '+fname+'...')
+            torchaudio.save(output_folder+fname, wavs[i].squeeze(), sr, encoding="PCM_S", bits_per_sample=16)
+
+    print("tracing "+str(input_folder)+" stems model...")
+    input_tensor=torch.rand(1,2,512,1024)
+    traced_model=torch.jit.trace(model, input_tensor)
+    print("saving "+str(input_folder)+" stems model...")
+    torch.jit.save(traced_model,str(input_folder)+'stems.pt')
+
+print("done !")
---
+++ unet.py
@@ -0,0 +1,142 @@
+import torch
+from torch import nn
+
+#from https://github.com/generalwave/spleeter.pytorch/blob/master/spleeter/models/keras_layer.py
+from torch.nn import functional
+from math import floor, ceil
+
+class Conv2dKeras(nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding='same', dilation=1, groups=1,
+                 bias=True, padding_mode='zeros'):
+        super(Conv2dKeras, self).__init__(
+            in_channels, out_channels, kernel_size, stride,
+            0, dilation, groups,
+            bias, padding_mode)
+        self.keras_mode = padding
+
+    def _padding_size(self, size, idx):
+        output = torch.div(size[idx] + self.stride[idx] - 1, self.stride[idx], rounding_mode='floor')
+        padding = (output - 1) * self.stride[idx] + (self.kernel_size[idx] - 1) * self.dilation[idx] + 1 - size[idx]
+        padding = max(0, padding)
+        return padding
+
+    def forward(self, x):
+        if self.keras_mode == 'same':
+            size = x.shape[2:]
+            row = self._padding_size(size, 0)
+            col = self._padding_size(size, 1)
+            x = functional.pad(x, [floor(col / 2), ceil(col / 2), floor(row / 2), ceil(row / 2)])
+
+        return super(Conv2dKeras, self).forward(x)
+
+
+class ConvTranspose2dKeras(nn.ConvTranspose2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, output_padding=0, groups=1, bias=True,
+                 dilation=1, padding_mode='zeros'):
+        assert output_padding == 1
+        super(ConvTranspose2dKeras, self).__init__(
+            in_channels, out_channels, kernel_size, 1,
+            dilation * (kernel_size - 1), 0, groups, bias,
+            dilation, padding_mode)
+
+        self.keras_kernel_size = kernel_size
+        self.keras_stride = stride
+        self.keras_padding = padding
+        self.keras_output_padding = output_padding
+        self.keras_dilation = dilation
+
+    def forward(self, x, output_size=None):
+        if self.padding_mode != 'zeros':
+            raise ValueError('Only `zeros` padding mode is supported for ConvTranspose2d')
+
+        assert output_size is None
+        output_padding = self.keras_output_padding
+
+        padding = self.keras_dilation * (self.keras_kernel_size - 1) - self.keras_padding
+
+        b, c, h, w = x.shape
+        h = h + (h - 1) * (self.keras_stride - 1) + 2 * padding + output_padding
+        w = w + (w - 1) * (self.keras_stride - 1) + 2 * padding + output_padding
+        hs, he = padding + output_padding, h - padding
+        ws, we = padding + output_padding, w - padding
+        newx = torch.zeros((b, c, h, w), dtype=x.dtype, device=x.device)
+        newx[:, :, hs:he:self.keras_stride, ws:we:self.keras_stride] = x
+
+        return functional.conv_transpose2d(
+            newx, self.weight, self.bias, self.stride, self.padding,
+            0, self.groups, self.dilation)
+
+
+#from https://github.com/tuan3w/spleeter-pytorch/
+#modified to support keras block (see above) and elu activation
+def down_block(in_filters, out_filters, elu=False):
+    return Conv2dKeras(in_filters, out_filters, kernel_size=5,
+                     stride=2, padding='same',
+                     ), nn.Sequential(
+        nn.BatchNorm2d(out_filters, track_running_stats=True, eps=1e-3, momentum=0.01),
+        nn.ELU(alpha=1) if elu else nn.LeakyReLU(0.2)
+    )
+
+
+def up_block(in_filters, out_filters, dropout=False, elu=False):
+    layers = [
+        ConvTranspose2dKeras(in_filters, out_filters, kernel_size=5,
+                           stride=2, padding=2, output_padding=1
+                           ),
+        nn.ELU(alpha=1) if elu else nn.ReLU(),
+        nn.BatchNorm2d(out_filters, track_running_stats=True, eps=1e-3, momentum=0.01)
+    ]
+    if dropout:
+        layers.append(nn.Dropout(0.5))
+
+    return nn.Sequential(*layers)
+
+
+class UNet(nn.Module):
+    def __init__(self, elu=False):
+        super(UNet, self).__init__()
+        self.down1_conv, self.down1_act = down_block(2, 16, elu)
+        self.down2_conv, self.down2_act = down_block(16, 32, elu)
+        self.down3_conv, self.down3_act = down_block(32, 64, elu)
+        self.down4_conv, self.down4_act = down_block(64, 128, elu)
+        self.down5_conv, self.down5_act = down_block(128, 256, elu)
+        self.down6_conv, self.down6_act = down_block(256, 512, elu)
+
+        self.up1 = up_block(512, 256, dropout=False, elu=elu)
+        self.up2 = up_block(512, 128, dropout=False, elu=elu)
+        self.up3 = up_block(256, 64, dropout=False, elu=elu)
+        self.up4 = up_block(128, 32, elu=elu)
+        self.up5 = up_block(64, 16, elu=elu)
+        self.up6 = up_block(32, 1, elu=elu)
+        self.up7 = nn.Sequential(Conv2dKeras(1, 2, kernel_size=4, dilation=2, padding='same')) #must also be sequential to properly assign weights
+
+    def forward(self, x):
+        d1_conv = self.down1_conv(x)
+        d1 = self.down1_act(d1_conv)
+
+        d2_conv = self.down2_conv(d1)
+        d2 = self.down2_act(d2_conv)
+
+        d3_conv = self.down3_conv(d2)
+        d3 = self.down3_act(d3_conv)
+
+        d4_conv = self.down4_conv(d3)
+        d4 = self.down4_act(d4_conv)
+
+        d5_conv = self.down5_conv(d4)
+        d5 = self.down5_act(d5_conv)
+
+        d6_conv = self.down6_conv(d5)
+        d6 = self.down6_act(d6_conv)
+
+        u1 = self.up1(d6_conv)
+        u2 = self.up2(torch.cat([d5_conv, u1], axis=1))
+        u3 = self.up3(torch.cat([d4_conv, u2], axis=1))
+        u4 = self.up4(torch.cat([d3_conv, u3], axis=1))
+        u5 = self.up5(torch.cat([d2_conv, u4], axis=1))
+        u6 = self.up6(torch.cat([d1_conv, u5], axis=1))
+        u7 = self.up7(u6)
+        return u7
+
